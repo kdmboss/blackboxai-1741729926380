ba64b3f3047c58b032b150bbfe6d695ee73f5990
kdmboss <malireddykarthikeya@gmail.com>
2025-03-11 17:52:10 -0400

    Add fintech-app/server.py 1741729930639

---
+++ fintech-app/server.py
@@ -0,0 +1,15 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+class Handler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=os.path.dirname(os.path.abspath(__file__)), **kwargs)
+
+def run(server_class=HTTPServer, handler_class=Handler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server running at http://localhost:{port}/")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
